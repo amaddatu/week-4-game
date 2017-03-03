3988b66b247efef03a85b9c82202f9b2a35d8d9d
Anthony <amaddatu@gmail.com>
2017-03-03 08:23:56 -0500

    Made some minor changes to fix style.
    I changed the giphy and and the giphy interval to better match the music.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -247,7 +247,7 @@ function titleExpander(){
 // Game
 $(document).ready(function(){
     newGame();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=fast+cars&limit=100&api_key=dc6zaTOxFJmzC"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=exotic+cars&limit=100&api_key=dc6zaTOxFJmzC"
     $.ajax({
         url: queryURL
         , method: 'GET'
@@ -260,7 +260,7 @@ $(document).ready(function(){
             }
     });
 
-    giphyIntervalTimer = createCircuitTimer(5000);
+    giphyIntervalTimer = createCircuitTimer(7110);
     //giphyIntervalTimer.start(giphyChange);
     
     musicIntervalTimer = createCircuitTimer(187547);
