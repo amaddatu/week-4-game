feb009b77a88c62c59c535e378342ae0ea3cdd60
Anthony <amaddatu@gmail.com>
2017-03-03 01:52:24 -0500

    Add music with timers and crystal reactions
    Crystals now react to music.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,6 +11,12 @@ var targetNumber, runningTotal;
 var giphyIntervalTimer = null;
 var giphyResponse = null;
 
+var musicIntervalTimer = null;
+var music = new Audio('assets/music/saintsrow4.mp3');
+var musicTimers = [];
+var musicCSS = "20%";
+var musicCSSLength = 500;
+
 
 // --------------------------------------------------------
 // Object Constructor
@@ -32,6 +38,27 @@ function createCircuitTimer(timeInterval){
             //clue: remember the booleans!!!
             if(this.running === true){
                 clearInterval(this.interval);
+                this.running = false;
+            }
+        }
+    };
+};
+
+//function to create simple timers or "simply smart timers"
+function createSimpleTimer(timeInterval){
+    return {
+        time: timeInterval
+        , timer: null
+        , running: false
+        , start: function(callMe){
+            if(this.running !== true){
+                this.timer = setTimeout(callMe, this.time);
+                this.running = true;
+            }
+        }
+        , stop: function(){
+            if(this.running === true){
+                clearTimeout(this.timer);
                 this.running = false;
             }
         }
@@ -98,12 +125,96 @@ function doGameLogic() {
         newGame();
     }
 }
+//changes the giphy
 function giphyChange(){
     console.log('giphy changed');
     if(giphyResponse != null)
     {
         $('body').css('background-image','url(' + giphyResponse.data[generateRandom(0, giphyResponse.data.length-1)].images.original.url + ')');
     }
+}
+//plays the music
+function musicChange(){
+    console.log('music changed');
+    if(music.duration > 0 && !music.paused){
+        music.pause();
+        music.currentTime = 0;
+        music.play();
+    }
+    else{
+        music.play();
+    }
+    musicTimerReset();
+}
+
+//creates all timers for music reactions
+function musicTimerReset(){
+    for(var i = 0; i < musicTimers.length; i++){
+        musicTimers.stop();
+    }
+    musicTimers = [];
+    /*
+14.753s
+28.979s
+? - 14.226s
+43.205s soft
+57.417s
+? - 14.212ss
+? avg 14.219s
+1m 11.636s
+1m 25.855s soft
+1m 40.407s soft
+1m 54.293s
+2m 8.512s
+2m 15.633s
+2m 22.753s
+2m 29.863s
+2m 36.95s
+2m 44.084s
+2m 51.169s
+3m 5.388s soft
+    */
+
+    musicCreateTimer(200);
+    musicCreateTimer(14353);
+    musicCreateTimer(28579);
+    musicCreateTimer(42805);
+    musicCreateTimer(57017);
+    musicCreateTimer(71236);
+    musicCreateTimer(85455);
+    musicCreateTimer(100007);
+    musicCreateTimer(113893);
+    musicCreateTimer(128112);
+    musicCreateTimer(135233);
+    musicCreateTimer(142353);
+    musicCreateTimer(149463);
+    musicCreateTimer(156550);
+    musicCreateTimer(163684);
+    musicCreateTimer(170769);
+    musicCreateTimer(184988);
+}
+//creations reaction timer
+function musicCreateTimer(microSec){
+    var mTime = createSimpleTimer(microSec);
+    mTime.start(musicReact);
+}
+function musicCreateTimer2(microSec){
+    var mTime = createSimpleTimer(microSec);
+    mTime.start(musicReact2);
+}
+//music reaction
+function musicReact(){
+    $('.crystal').css('padding', musicCSS);
+    setTimeout(function(){
+        $('.crystal').attr('style', '');
+    }, musicCSSLength);
+}
+//alternate music version
+function musicReact2(){
+    $('.crystal').css('padding', musicCSS);
+    setTimeout(function(){
+        $('.crystal').attr('style', '');
+    }, musicCSSLength/2);
 }
 
 
@@ -128,6 +239,9 @@ $(document).ready(function(){
     giphyIntervalTimer = createCircuitTimer(5000);
     giphyIntervalTimer.start(giphyChange);
     
+    musicIntervalTimer = createCircuitTimer(187547);
+    musicIntervalTimer.start(musicChange);
+    musicChange();
     //http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
 });
 
